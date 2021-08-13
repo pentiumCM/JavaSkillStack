8902778e5ededf3a225efb744bc83b11e2e2417b
pentiumCM <842679178@qq.com>
2021-08-13 23:54:31 +0800

    tag:Java并发

--- src/main/java/indi/pentiumcm/string/Test.java
+++ src/main/java/indi/pentiumcm/string/Test.java
@@ -1,5 +1,9 @@
 package indi.pentiumcm.string;
 
+import jnr.ffi.annotations.In;
+
+import java.util.*;
+
 /**
  * @projName: JavaSkillStack
  * @packgeName: indi.pentiumcm.string
@@ -11,26 +15,15 @@ package indi.pentiumcm.string;
  */
 public class Test {
 
+    private String name;
+
+    public synchronized void setName(String name) {
+        this.name = name;
+    }
 
     public static void main(String[] args) {
 
-        String a = "abc";
-        String b = "abc";
-
-        boolean f1 = (a == b);
-        System.out.println(f1);
-
-
-        String x = new String("abc");
-        String y = new String("abc");
-        boolean f2 = (x == y);
-        System.out.println(f2);
-
-        boolean f3 = (a == x);
-        System.out.println(f3);
-
-
-        System.out.println(x);
+        List<Integer> list = new ArrayList<>();
     }
 
 }
